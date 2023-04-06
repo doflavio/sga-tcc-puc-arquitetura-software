5a8e9f55ac6f4b640a50b02ae51bd43fc281f82a
Flavio <doflavio@gmail.com>
2023-04-06 14:28:01 -0300

    Configurando o servico de usuario

--- sg-user/src/main/java/io/github/doflavio/sguser/SgUserApplication.java
+++ sg-user/src/main/java/io/github/doflavio/sguser/SgUserApplication.java
@@ -1,13 +1,31 @@
 package io.github.doflavio.sguser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class SgUserApplication {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgUserApplication.class, args);
 	}
+	
+	/*
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));		
+	}
+	*/
 
 }
+
+
+	
+
---
+++ sg-user/src/main/java/io/github/doflavio/sguser/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package io.github.doflavio.sguser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.doflavio.sguser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+
+}
