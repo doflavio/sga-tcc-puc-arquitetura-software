183106869ed0792ac468b4e99e703e01484309cc
Flavio <doflavio@gmail.com>
2023-04-06 15:15:43 -0300

    Criando projeto config server - centralizar configuracoes

---
+++ sg-config-server/src/main/java/io/github/doflavio/sgconfigserver/SgConfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgConfigServerApplication.class, args);
+	}
+
+}
---
+++ sg-config-server/src/test/java/io/github/doflavio/sgconfigserver/SgConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
