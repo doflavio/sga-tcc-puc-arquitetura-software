d025828992a44902b5e705c455a5f88c35460c3a
Flavio <doflavio@gmail.com>
2023-04-06 14:30:21 -0300

    Criando projeto Eureka server

---
+++ sg-eureka-server/src/main/java/io/github/doflavio/sgeurekaserver/SgEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgEurekaServerApplication.class, args);
+	}
+
+}
---
+++ sg-eureka-server/src/test/java/io/github/doflavio/sgeurekaserver/SgEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
