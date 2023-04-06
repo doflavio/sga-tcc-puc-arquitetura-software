f6fbeafbd070d53d6194f94d99759e99e5637fa6
Flavio <doflavio@gmail.com>
2023-04-06 13:24:11 -0300

    Criando o projeto sg-user

---
+++ sg-user/src/main/java/io/github/doflavio/sguser/SgUserApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sguser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgUserApplication.class, args);
+	}
+
+}
