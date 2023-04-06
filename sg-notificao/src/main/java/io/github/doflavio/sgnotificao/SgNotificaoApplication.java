d33d8e07f28b323edbd12f0e9892f85e9d91c56e
Flavio <doflavio@gmail.com>
2023-04-06 14:54:00 -0300

    Configurando o servico de notificao para registrar no eureka server

--- sg-notificao/src/main/java/io/github/doflavio/sgnotificao/SgNotificaoApplication.java
+++ sg-notificao/src/main/java/io/github/doflavio/sgnotificao/SgNotificaoApplication.java
@@ -2,7 +2,9 @@ package io.github.doflavio.sgnotificao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SgNotificaoApplication {
 
