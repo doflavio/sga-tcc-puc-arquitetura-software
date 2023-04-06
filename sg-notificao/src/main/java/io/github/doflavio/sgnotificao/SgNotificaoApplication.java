6279c567c61c4d61fe2c99df675222d53413299b
Flavio <doflavio@gmail.com>
2023-04-06 14:42:29 -0300

    Criando projeto de notificacao

---
+++ sg-notificao/src/main/java/io/github/doflavio/sgnotificao/SgNotificaoApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgnotificao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgNotificaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgNotificaoApplication.class, args);
+	}
+
+}
---
+++ sg-notificao/src/test/java/io/github/doflavio/sgnotificao/SgNotificaoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgnotificao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgNotificaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
