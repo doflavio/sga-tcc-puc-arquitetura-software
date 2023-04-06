f10882613c373fd02b33ffea52522f714210f424
Flavio <doflavio@gmail.com>
2023-04-06 13:16:02 -0300

    Criando projeto sg-auth

---
+++ sg-auth/src/test/java/io/github/doflavio/sgauth/SgAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
