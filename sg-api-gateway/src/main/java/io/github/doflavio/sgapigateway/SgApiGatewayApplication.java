91be90e1acd5115ea84aa7d62512ae8b12d35169
Flavio <doflavio@gmail.com>
2023-04-07 00:30:10 -0300

    Criando api gateway

---
+++ sg-api-gateway/src/main/java/io/github/doflavio/sgapigateway/SgApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgApiGatewayApplication.class, args);
+	}
+
+}
