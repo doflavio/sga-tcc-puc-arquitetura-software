1eafaac850ec778a426943407e33c3afcae9d759
Flavio <doflavio@gmail.com>
2023-04-07 11:49:46 -0300

    Criando o projeto oath - Authorization server

---
+++ sg-oauth/src/main/java/io/github/doflavio/sgoauth/SgOauthApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgOauthApplication.class, args);
+	}
+
+}
---
+++ sg-oauth/src/test/java/io/github/doflavio/sgoauth/SgOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
