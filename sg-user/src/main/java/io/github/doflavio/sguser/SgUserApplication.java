282de1b26f43c2ca67ae1635be7ad73a2e21b379
Flavio <doflavio@gmail.com>
2023-04-07 03:19:01 -0300

    Ajustando configuracao do api gateway

--- sg-user/src/main/java/io/github/doflavio/sguser/SgUserApplication.java
+++ sg-user/src/main/java/io/github/doflavio/sguser/SgUserApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SgUserApplication {
 	
