ca2d884c94ca4d888b9049f8ed4ec786cccf4346
Flavio <doflavio@gmail.com>
2023-04-08 20:42:31 -0300

    Incluindo projeto de autenticacao e autorizacao

---
+++ sga-auth-authorization/src/main/java/io/github/doflavio/sgaauthauthorization/SgaAuthAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.sgaauthauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaAuthAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaAuthAuthorizationApplication.class, args);
+	}
+
+}
