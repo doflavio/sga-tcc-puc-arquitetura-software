1ee43d1159ab6903f98fe496af68c4908af4509a
Flavio <doflavio@gmail.com>
2023-04-22 01:56:17 -0300

    Criando bean para utlizar modelMapper

--- sg-monitoramento-seguranca/src/main/java/io/github/doflavio/sgmonitoramentoseguranca/config/AppConfig.java
+++ sg-monitoramento-seguranca/src/main/java/io/github/doflavio/sgmonitoramentoseguranca/config/AppConfig.java
@@ -1,5 +1,6 @@
 package io.github.doflavio.sgmonitoramentoseguranca.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +16,16 @@ public class AppConfig {
 	@Bean
 	public void inicializarDB() {
 		this.dbService.instanciaDB();
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		var modelMapper = new ModelMapper();
+		
+		//modelMapper.createTypeMap(, null)
+		//mapper.setSerializationInclusion(Include.NON_NULL);
+		modelMapper.getConfiguration().setAmbiguityIgnored(true);
+		
+		return modelMapper;
 	}
 }
