2a3be469e72d177ed7d40dbbc2c769a0ab6f6401
Flavio <doflavio@gmail.com>
2023-04-30 23:48:15 -0300

    Removendo hr-worker - nao utilizado

--- hr-worker/src/main/java/com/marcar/hrworker/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marcar.hrworker.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.marcar.hrworker.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Long>{
-
-}
